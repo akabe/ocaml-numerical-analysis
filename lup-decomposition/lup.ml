7b52ba0d5384f1f0861de41b00fa72a51a2af5eb
Akinori ABE <abe@kb.ecei.tohoku.ac.jp>
2015-04-28 20:40:02 +0900

    Improve code of LUP decomposition

--- lup-decomposition/lup.ml
+++ lup-decomposition/lup.ml
@@ -2,6 +2,7 @@
 
     [MIT License] Copyright (C) 2015 Akinori ABE
 *)
+
 open Format
 
 module Array = struct
@@ -13,6 +14,12 @@ module Array = struct
     let m = length a in
     let n = if m = 0 then 0 else length a.(0) in
     (m, n)
+
+  (** [swap x i j] swaps [x.(i)] and [x.(j)]. *)
+  let swap x i j =
+    let tmp = x.(i) in
+    x.(i) <- x.(j);
+    x.(j) <- tmp
 end
 
 (** [foldi f init i j] is [f (... (f (f init i) (i+1)) ...) j]. *)
@@ -42,11 +49,6 @@ let lup a0 =
   let r = min m n in
   let p = Array.init m (fun i -> i) in (* permutation indices *)
   let lu = Array.make_matrix m n 0.0 in
-  let swap x i j =
-    let tmp = x.(i) in
-    x.(i) <- x.(j);
-    x.(j) <- tmp
-  in
   let aux i j q = a.(i).(j) -. sumi (fun k -> lu.(i).(k) *. lu.(k).(j)) 0 q in
   let get_pivot j =
     maxi (fun i -> abs_float (aux i j (min (i-1) (r-1)))) j (m-1)
@@ -54,37 +56,16 @@ let lup a0 =
   for j = 0 to r - 1 do
     (* pivot selection (swapping rows) *)
     let j' = get_pivot j in
-    if j <> j' then begin
-      swap p j j';
-      swap a j j';
-      swap lu j j'
-    end;
+    if j <> j' then Array.(swap p j j' ; swap a j j' ; swap lu j j');
     (* Compute LU decomposition *)
     for i = 0 to j do lu.(i).(j) <- aux i j (i-1) done;
     for i = j+1 to m-1 do lu.(i).(j) <- aux i j (j-1) /. lu.(j).(j) done
   done;
   (* Compute the right block in the upper trapezoidal matrix *)
-  if m < n then
-    for j = r to n-1 do
-      for i = 0 to r-1 do lu.(i).(j) <- aux i j (i-1) done
-    done;
+  for j = r to n-1 do
+    for i = 0 to r-1 do lu.(i).(j) <- aux i j (i-1) done
+  done;
   (p, lu)
-
-(** Separate a returned matrix of [lup] into upper and lower triangular
-    matrices. *)
-let make_lu lu =
-  let m, n = Array.matrix_size lu in
-  let r = min m n in
-  let l = Array.init_matrix m r
-      (fun i j -> if i = j then 1.0 else if i > j then lu.(i).(j) else 0.0) in
-  let u = Array.init_matrix r n
-      (fun i j -> if i <= j then lu.(i).(j) else 0.0) in
-  (l, u)
-
-(** Construct a permutation matrix from given permutation indices. *)
-let make_p p =
-  let m = Array.length p in
-  Array.init_matrix m m (fun i j -> if i = p.(j) then 1.0 else 0.0)
 
 (** Matrix multiplication *)
 let gemm x y =
@@ -111,9 +92,17 @@ let () =
       [|-2.;-1.;-1.; 4.; 6.|]
     |] in
   let p, lu = lup a in
-  let l, u = make_lu lu in
-  let p = make_p p in
-  let a' = gemm p (gemm l u) in
+  let m, n = Array.matrix_size lu in
+  let r = min m n in
+  let l = (* a lower trapezoidal matrix *)
+    Array.init_matrix m r
+      (fun i j -> if i > j then lu.(i).(j) else if i = j then 1.0 else 0.0) in
+  let u = (* an upper trapezoidal matrix *)
+    Array.init_matrix r n
+      (fun i j -> if i <= j then lu.(i).(j) else 0.0) in
+  let p = (* a permutation matrix *)
+    Array.init_matrix m m (fun i j -> if i = p.(j) then 1.0 else 0.0) in
+  let a' = gemm p (gemm l u) in (* ['a] is equal to [a]. *)
   print_mat "matrix A" a;
   print_mat "matrix L" l;
   print_mat "matrix U" u;
